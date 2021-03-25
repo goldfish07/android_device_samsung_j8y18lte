5e2e6c295c961e889d2ba170f67736ee7af64de8
Ayush Bisht <ayushbisht5663@gmail.com>
2021-03-25 20:10:29 +0530

    j8y18lte: specify device model

--- lineage_j8y18lte.mk
+++ lineage_j8y18lte.mk
@@ -25,6 +25,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := lineage_j8y18lte
 PRODUCT_DEVICE := j8y18lte
+PRODUCT_MODEL := SM-J810G
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
