0121def0145553880ddc322392a11cfbbcc753d7
Ayush Bisht <ayushbisht5663@gmail.com>
2021-12-12 03:42:51 +0530

    j8y18lte: update changes for carbon
    Signed-off-by: Ayush Bisht <ayushbisht5663@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,10 +13,10 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_j8y18lte.mk
+    $(LOCAL_DIR)/carbon_j8y18lte.mk
 
 
 COMMON_LUNCH_CHOICES := \
-     lineage_j8y18lte-userdebug \
-     lineage_j8y18lte-eng \
-     lineage_j8y18lte-user
+     carbon_j8y18lte-userdebug \
+     carbon_j8y18lte-eng \
+     carbon_j8y18lte-user
