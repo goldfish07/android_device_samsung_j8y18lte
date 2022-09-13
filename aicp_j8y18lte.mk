3dcec7e0d83c3cf9faa289fbdb850a5ba17768d9
Ayush Bisht <ayushbisht5663@gmail.com>
2022-09-13 19:16:20 +0530

    j8y18lte: aicp init
    Signed-off-by: Ayush Bisht <ayushbisht5663@gmail.com>

--- aicp_j8y18lte.mk
+++ aicp_j8y18lte.mk
@@ -19,11 +19,18 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit common Lineage phone.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit common aicp phone.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="goldfish07(Ayush Bisht)"
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := lineage_j8y18lte
+PRODUCT_NAME := aicp_j8y18lte
 PRODUCT_DEVICE := j8y18lte
 PRODUCT_MODEL := SM-J810G
 PRODUCT_BRAND := samsung
