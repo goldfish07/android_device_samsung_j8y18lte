dc6e567813f5b29bf4e11d1dc52ad1fbf312a004
Ayush Bisht <ayushbisht5663@gmail.com>
2022-10-07 08:11:08 +0530

    j8y18lte: adapt changes for Havoc-OS
    Signed-off-by: Ayush Bisht <ayushbisht5663@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,10 +13,10 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_j8y18lte.mk
+    $(LOCAL_DIR)/havoc_j8y18lte.mk
 
 
 COMMON_LUNCH_CHOICES := \
-     lineage_j8y18lte-userdebug \
-     lineage_j8y18lte-eng \
-     lineage_j8y18lte-user
+     havoc_j8y18lte-userdebug \
+     havoc_j8y18lte-eng \
+     havoc_j8y18lte-user
