d3beb7e0015780ac061346a8657075fb61a274b1
Ayush Bisht <ayushbisht5663@gmail.com>
2022-10-10 19:21:57 +0530

    j8y18lte: adapt changes for bliss https://github.com/BlissRoms/platform_manifest
    Signed-off-by: Ayush Bisht <ayushbisht5663@gmail.com>

--- device.mk
+++ device.mk
@@ -146,7 +146,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
     libmm-qcamera \
-    Snap
+    GCamGOPrebuilt
 
 # Display
 PRODUCT_PACKAGES += \
