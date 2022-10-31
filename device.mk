13850f1d2eef7a712cc0e13ad2901eeea0f6bba4
Ayush <ayushbisht5663@gmail.com>
2022-10-31 08:10:12 +0530

    j8y18lte: Drop com.dsi.ant.antradio_library
    No longer shipped by default with lineage-19.0.
    
    Change-Id: Idd16f81ed0bfe782967e8efd0f960d1f598d1fb7
    Signed-off-by: Ayush <ayushbisht5663@gmail.com>

--- device.mk
+++ device.mk
@@ -64,7 +64,6 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
@@ -92,9 +91,7 @@ PRODUCT_COPY_FILES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
+    AntHalService
 
 # Audio
 PRODUCT_PACKAGES += \
